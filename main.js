3e4b6c56a5281ecd7dedf494eb5c8ff7e907d3e7
Kacper Augsutynik <kacperaugustynik96@gmail.com>
2020-09-16 21:33:11 +0200

    change name class in subpage to BEM methodology

--- main.js
+++ main.js
@@ -3,3 +3,11 @@ const name = "Kacper";
 const age = 24;
 console.log(`Siema jestem ${name} i mama ${age} lata.`);
 //alert(`Siema jestem ${name} i mama ${age} lata.`)
+
+const title = document.querySelector('.first-section__title');
+console.log(title.innerHTML);
+console.log(title);
+
+title.innerHTML = 'elo elo';
+console.log(title.innerHTML);
+console.log(title);
