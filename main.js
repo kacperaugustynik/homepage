5d21ebff4da64032e2a94b12ae8433ee7785d7dc
Kacper Augsutynik <kacperaugustynik96@gmail.com>
2020-09-21 20:16:11 +0200

    changes is js

--- main.js
+++ main.js
@@ -4,10 +4,28 @@ const age = 24;
 console.log(`Siema jestem ${name} i mama ${age} lata.`);
 //alert(`Siema jestem ${name} i mama ${age} lata.`)
 
-const title = document.querySelector('.first-section__title');
+const title = document.querySelector('.about-me__title');
 console.log(title.innerHTML);
 console.log(title);
 
 title.innerHTML = 'elo elo';
 console.log(title.innerHTML);
 console.log(title);
+
+function greet (name, age) {
+console.log(`Czesc, jestem ${name} i mam ${age}`);
+}
+
+greet(`Kacper`,24);
+
+const greetSec = (name,age) => console.log(`Czesc, jestem ${name} i mam ${age}`);
+
+greetSec(`Kacper`,25)
+
+function contentChanger (querySelector, content) {
+
+    const element = document.querySelector(querySelector);
+    element.innerHTML = content;
+}
+
+contentChanger(`.main-header__description`,`WTF`);
